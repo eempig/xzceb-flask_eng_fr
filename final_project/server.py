9660adf85640c7153d0fdf4051b4e3c988b37341
eempig <59047479+eempig@users.noreply.github.com>
2022-09-20 22:05:07 +0800

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -8,12 +8,14 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
+    translatedText = translator.english_to_french(textToTranslate)
     return "Translated text to French"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
+    translatedText = translator.french_to_english(textToTranslate)
     return "Translated text to English"
 
 @app.route("/")
